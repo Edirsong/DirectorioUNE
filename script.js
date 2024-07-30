718c647c6b7778b36b46179c45f9c328498d9a08
Edirsong <123476716+Edirsong@users.noreply.github.com>
2024-07-29 23:08:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var gData;
-var url =  'https://xxxxx/exec' 
+var url =  'https://script.google.com/macros/s/AKfycbw7CmsLI5uYe4mJyWHQGSuup_NWKpGSmIdAjTUgarfrUo_tIF2KgG6n56rs4pvmE4gf/exec' 
 
   function getDataAPI() {  
     $('#d_table').html('<div class="col">Please Wait.....</div>') 
